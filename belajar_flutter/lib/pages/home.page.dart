b1ce494566c441a7addc4e318b8ec9cd7eae2343
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-29 20:46:52 +0700

    news by components

--- belajar_flutter/lib/pages/home.page.dart
+++ belajar_flutter/lib/pages/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_flutter/components/news.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Berita terbaru saat ini'),
+      ),
+      body: const SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            News(
+              title: 'Ini judul',
+              image:
+                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
+            ),
+            News(
+              title: 'Ini judul',
+              image:
+                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
+            ),
+            News(
+              title: 'Ini judul',
+              image:
+                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
+            ),
+            News(
+              title: 'Ini judul',
+              image:
+                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
