7bb78144de5a4632a4c7919de2c9c54352bd1e23
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-29 22:28:40 +0700

    dynamic headlines news

--- belajar_flutter/lib/main.dart
+++ belajar_flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:belajar_flutter/pages/home.page.dart';
+import 'package:belajar_flutter/providers/news.provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => NewsProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'News App - FINAL TASK',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- belajar_flutter/lib/pages/home.page.dart
+++ belajar_flutter/lib/pages/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:belajar_flutter/components/news.dart';
+import 'package:belajar_flutter/providers/news.provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,39 +12,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    Provider.of<NewsProvider>(context, listen: false).getTopNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Berita terbaru saat ini'),
-      ),
-      body: const SingleChildScrollView(
-          child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            News(
-              title: 'Ini judul',
-              image:
-                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
-            ),
-            News(
-              title: 'Ini judul',
-              image:
-                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
-            ),
-            News(
-              title: 'Ini judul',
-              image:
-                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
-            ),
-            News(
-              title: 'Ini judul',
-              image:
-                  'https://3.bp.blogspot.com/-hDk7AwXj5Os/UYBDZdkmG1I/AAAAAAAAEzQ/_rFohGALRPE/s1600/Gambar-Ayam-Jantan.jpg',
-            ),
-          ],
+    return Consumer<NewsProvider>(
+        builder: (BuildContext context, news, Widget? child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Berita terbaru saat ini'),
         ),
-      )),
-    );
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              news.isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Column(
+                      children: [
+                        ...news.resNews!.articles!.map(
+                          (e) => News(
+                            title: e.title ?? 'No Title',
+                            image: e.urlToImage ??
+                                'https://via.placeholder.com/150x100.png?text=No+Image',
+                          ),
+                        )
+                      ],
+                    ),
+            ],
+          ),
+        )),
+      );
+    });
   }
 }
